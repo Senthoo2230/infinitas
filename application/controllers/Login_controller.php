fd971b393ad0dcdfd1337eb14da7dcd175f80540
Senthooran <30416138+Senthoo2230@users.noreply.github.com>
2023-10-12 16:19:40 +0530

    first commit

---
+++ application/controllers/Login_controller.php
@@ -0,0 +1,15 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+        
+class Login_controller extends CI_Controller {
+
+public function index()
+{
+    echo 4;
+}
+        
+}
+        
+/* End of file  Login_controller.php */
+        
+                            
---
+++ application/models/Login_model.php
@@ -0,0 +1,14 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+                        
+class Login_model extends CI_Model 
+{
+    public function select()
+    {
+
+    }                        
+                        
+}
+
+
+/* End of file Login_model.php and path \application\models\Login_model.php */
